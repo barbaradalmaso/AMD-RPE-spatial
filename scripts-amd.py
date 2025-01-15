52c573cfd26519642b07aeab02414be10c13e783
Barbara Dalmaso <72207032+barbaradalmaso@users.noreply.github.com>
2025-01-15 18:48:56 -0300

    Update 15.01.25

--- scripts-amd.py
+++ scripts-amd.py
@@ -3,6 +3,8 @@
 ###################### ESTUDANTE BARBARA DALMASO #################
 
 # In[0.1]: Importar pacotes
+# pip install numpy pandas matplotlib scanpy seaborn scipy anndata scikit-learn
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,6 +15,7 @@ from scipy.io import mmread
 from scipy.sparse import csr_matrix
 import anndata as ad
 from sklearn.decomposition import PCA
+from scipy.stats import pearsonr
 
 
 # In[0.2]: Ajustar work-directory
@@ -25,8 +28,7 @@ os.chdir("/Users/barbaradalmaso/Desktop/AMD-RPE-spatial/Dados/")
 # 2. scRNA-seq espacial (sdata): Choi, J., Li, J., Ferdous, S. et al. Spatial organization of the mouse retina at single cell resolution by MERFISH. Nat Commun 14, 4929 (2023). https://doi.org/10.1038/s41467-023-40674-3
 
 # This file contains the raw MERFISH count matrix for six samples with 500 gene features. The "sampleid" column represents the unique sample ID, while the "region" column corresponds to the tissue section ID. The "majorclass" and "subclass" columns indicate annotated retinal cell types. Finally, the "center_x" and "center_y" columns provide the coordinates of the cell centers.
-sdata = sc.read_h5ad("VA45_integrated.h5ad") # https://zenodo.org/records/8144355 
-
+sdata = sc.read_h5ad("merfish_impute.h5ad") # https://zenodo.org/records/8144355 
 
 ##################### Processamento de dados de spatial scRNA-seq de retina saudável #############################
 # Criar DataFrame da matriz de expressão
@@ -35,27 +37,10 @@ expression_df = pd.DataFrame(
     index=sdata.obs.index,  # Índice das células
     columns=sdata.var.index  # Nomes dos genes
 )
-expression_df = pd.concat([sdata.obs, expression_df], axis=1) # Integrar df com metadados
-gene_list = expression_df.columns[11:]  # Seleciona apenas os nomes das colunas, que contém os nomes dos genes
+
+# Selecionar os genes e converter nomenclatura de camundongo para humano
+gene_list = expression_df.columns.tolist() # Seleciona apenas os nomes das colunas, que contém os nomes dos genes
 gene_list_human = [gene.upper() for gene in gene_list]  # Converter genes de camundongo para humano
-
-
-# Selecionar dados de cada amostra, escolher região com melhores tecidos: regiões de 0-7 e sample id de DV1-3 e TN1-3
-filtered_df = expression_df[(expression_df['sampleid'] == 'TN3') & (expression_df['region'] == 0)] # O melhor
-
-
-# Criar o scatter plot para essa região
-plt.figure(figsize=(50, 50))
-sns.scatterplot(
-    x='center_x', y='center_y', hue='subclass', 
-    data=filtered_df, palette='tab20', s=30, alpha=0.7, legend=False
-)
-plt.title('Distribuição Espacial das Subclasses de Células', fontsize=16)
-plt.xlabel('Coordenada X')
-plt.ylabel('Coordenada Y')
-plt.gca().invert_yaxis()
-
-plt.show()
 
 # In[1.1]: Download de datasets - scRNA-seq convencional
 ##################### Processamento de dados de scRNA-seq de retina saudável e degenerada #############################
@@ -110,17 +95,17 @@ control = sc.read_h5ad("/Users/barbaradalmaso/Desktop/AMD-RPE-spatial/Dados/GSE2
 # Primeiro, como observei que a maior parte das celulas possuem baixo count de genes, decidi filtrar 
 # as celulas com total count abaixo de 500 (em cada amostra)
 control.obs['total_counts'] = control.X.sum(axis=1).A1 if isinstance(control.X, csr_matrix) else control.X.sum(axis=1)
-control_filtered = control[(control.obs['total_counts'] > 500) & (control.obs['total_counts'] < 5000), :]
+control_filtered = control[(control.obs['total_counts'] > 1000) & (control.obs['total_counts'] < 10000), :]
 print(f"Número de células antes da filtragem: {control.n_obs}")
 print(f"Número de células após a filtragem: {control_filtered.n_obs}")
 
 dry.obs['total_counts'] = dry.X.sum(axis=1).A1 if isinstance(dry.X, csr_matrix) else dry.X.sum(axis=1)
-dry_filtered = dry[(dry.obs['total_counts'] > 500) & (dry.obs['total_counts'] < 5000), :]
+dry_filtered = dry[(dry.obs['total_counts'] > 1000) & (dry.obs['total_counts'] < 10000), :]
 print(f"Número de células antes da filtragem: {dry.n_obs}")
 print(f"Número de células após a filtragem: {dry_filtered.n_obs}")
 
 wet.obs['total_counts'] = wet.X.sum(axis=1).A1 if isinstance(wet.X, csr_matrix) else wet.X.sum(axis=1)
-wet_filtered = wet[(wet.obs['total_counts'] > 500) & (wet.obs['total_counts'] < 5000), :]
+wet_filtered = wet[(wet.obs['total_counts'] > 1000) & (wet.obs['total_counts'] < 10000), :]
 print(f"Número de células antes da filtragem: {wet.n_obs}")
 print(f"Número de células após a filtragem: {wet_filtered.n_obs}")
 
@@ -223,7 +208,136 @@ plt.tight_layout()
 plt.show()
 
 # In[1.3]: Análise exploratória scRNA-seq espacial
-sdata = sc.read_h5ad("/Users/barbaradalmaso/Desktop/AMD-RPE-spatial/Dados/VA45_integrated.h5ad")  # Seu arquivo de dados espaciais
+# Carregar os dados espaciais
+sdata = sc.read_h5ad("/Users/barbaradalmaso/Desktop/AMD-RPE-spatial/Dados/merfish_impute.h5ad")
+bulkdata = sc.read_h5ad("/Users/barbaradalmaso/Desktop/AMD-RPE-spatial/Dados/combined_patient_sc.h5ad")
 
-# Selecionar uma coluna de interesse para análise (pode ser uma coluna de expressão gênica ou outra)
-sdata = sdata.obs['']
+# Selecionar apenas os genes presentes na lista 'gene_list_expression' (geralmente da amostra dry)
+sdata.var.index = sdata.var.index.str.upper()
+gene_list_expression = bulkdata.var.index
+sdata_filtered_genes = sdata[:, sdata.var.index.isin(gene_list_expression)]
+
+# Filtrar as células com base no total de contagens de forma otimizada
+if isinstance(sdata_filtered_genes.X, csr_matrix):
+    sdata_filtered_genes.obs['total_counts'] = np.array(sdata_filtered_genes.X.sum(axis=1)).flatten()
+else:
+    sdata_filtered_genes.obs['total_counts'] = sdata_filtered_genes.X.sum(axis=1)
+
+# Filtrar células com total_count > 500
+sdata_filtered = sdata_filtered_genes[(sdata_filtered_genes.obs['total_counts'] > 1000) & (sdata_filtered_genes.obs['total_counts'] < 10000),:]
+
+
+# Exibir informações sobre a filtragem
+print(f"Dimensão original de sdata: {sdata.shape}")
+print(f"Dimensão após a filtragem de genes e total_counts > 500: {sdata_filtered.shape}")
+
+# Salvar o objeto combinado em um arquivo .h5ad
+output_path = "/Users/barbaradalmaso/Desktop/AMD-RPE-spatial/Dados/filtered_spatialdata.h5ad"
+sdata_filtered.write(output_path)
+
+###### Teste de normalidade ######
+# Histograma da contagem de genes por célula
+sns.set_context("talk", font_scale=1.2)  # Aumenta o tamanho das fontes
+sns.set_style("whitegrid")  # Adiciona grid para melhor visualização
+
+# Criar o gráfico com as contagens totais de cada célula após a filtragem
+plt.figure(figsize=(6, 6))
+sns.histplot(sdata_filtered.obs['total_counts'], bins=50, kde=False, color='red', linewidth=2)
+plt.title('Spatial data', fontsize=20, weight='bold')
+plt.xlabel('Total Counts', fontsize=16)
+plt.ylabel('Frequency', fontsize=16)
+plt.show()
+
+# Função para realizar PCA
+def perform_pca(data, gene_list, n_components=2):
+    # Extraindo a matriz de expressão para os genes de interesse
+    gene_expression_data = data[:, data.var.index.isin(gene_list_expression)].X
+    pca = PCA(n_components=n_components)
+    pca_result = pca.fit_transform(gene_expression_data)  # Realiza o PCA nos dados de expressão
+    explained_variance = pca.explained_variance_ratio_
+    return pca_result, explained_variance
+
+# Realizar PCA
+pca_result, pca_var = perform_pca(sdata_filtered, gene_list_expression)
+
+# Gráfico de PCA
+plt.figure(figsize=(8, 6))
+sns.scatterplot(x=pca_result[:, 0], y=pca_result[:, 1], alpha=0.7, color='red', edgecolor=None)
+
+# Título e rótulos
+plt.title(f'PCA - PC1: {pca_var[0]*100:.2f}% | PC2: {pca_var[1]*100:.2f}%', fontsize=16)
+plt.xlabel(f'PC1 ({pca_var[0]*100:.2f}%)', fontsize=14)
+plt.ylabel(f'PC2 ({pca_var[1]*100:.2f}%)', fontsize=14)
+
+# Exibição do gráfico
+plt.grid(True)
+plt.show()
+
+# In[2.1]: Finalizei as análises exploratórias. Verificamos até o momento que existe um padrão 
+# de distribuição de 'total counts' da expressão gênica nos diferentes datasets, com distribuição
+#  de poisson, conjuntamente com um PCA em forma de V. Agora vamos iniciar os testes de similaridade entre os datasets
+
+# Datasets
+sdata = sc.read_h5ad("/Users/barbaradalmaso/Desktop/AMD-RPE-spatial/Dados/filtered_spatialdata.h5ad")  # Arquivo de dados espaciais
+bulkdata = sc.read_h5ad("/Users/barbaradalmaso/Desktop/AMD-RPE-spatial/Dados/combined_patient_sc.h5ad") # Arquivo de dados scRNA-seq
+
+# Calcular a média de expressão para cada gene nos datasets 
+sdata_mean = sdata.X.mean(axis=0)
+bulkdata_mean = bulkdata.X.mean(axis=0)
+mean_expression = pd.DataFrame({
+    "gene": sdata.var.index,
+    "spatial_mean": np.array(sdata_mean).flatten(),
+    "bulk_mean": np.array(bulkdata_mean).flatten()
+})
+
+# Calcular o coeficiente de correlação de Pearson
+corr, p_value = pearsonr(mean_expression["spatial_mean"], mean_expression["bulk_mean"])
+print(f"Correlação de Pearson: {corr:.2f} (p-valor: {p_value:.2e})")
+
+# Plotar os dados
+plt.figure(figsize=(8, 6))
+plt.scatter(mean_expression["spatial_mean"], mean_expression["bulk_mean"], alpha=0.7, edgecolor='k')
+plt.title(f"Person's Correlation': {corr:.2f}", fontsize=20)
+plt.xlabel("Mean Expression - Spatial Data", fontsize=18)
+plt.ylabel("Mean Expression - Single cell Data", fontsize=18)
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.show()
+
+# Observei uma correlação muito baixa entre a expressão global de genes
+# Agora vou fzer t-SNE
+# 1. Combinar os dados
+sdata_adata = sdata.to_df()  # Converte AnnData para DataFrame
+bulkdata_adata = bulkdata.to_df()
+
+# Criar labels para identificação do dataset de origem
+sdata_adata['dataset'] = 'sdata'
+bulkdata_adata['dataset'] = 'bulkdata'
+
+# Combinar os datasets
+combined_data = pd.concat([sdata_adata, bulkdata_adata], axis=0)
+
+# Separar os dados de expressão e labels
+expression_matrix = combined_data.drop(columns=['dataset'])
+labels = combined_data['dataset']
+
+# 2. Criar AnnData para análise
+combined_adata = sc.AnnData(expression_matrix)
+combined_adata.obs['dataset'] = labels
+
+# 3. Pré-processamento
+sc.pp.normalize_total(combined_adata, target_sum=1e4)
+sc.pp.log1p(combined_adata)
+sc.pp.scale(combined_adata)
+
+# 4. PCA para redução de dimensionalidade
+sc.tl.pca(combined_adata)
+
+# 5. t-SNE
+sc.pp.neighbors(combined_adata, n_neighbors=5, n_pcs=10)
+sc.tl.tsne(combined_adata)
+
+# 6. Plotar t-SNE com cores baseadas no dataset
+sc.pl.tsne(combined_adata, color='dataset', title='t-SNE Geral - Datasets')
+
+# 7. Plotar PCA
+sc.pl.pca(combined_adata, color='dataset', title='PCA Geral - Datasets')
